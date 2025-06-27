fdd0aecab7192e5cf7798776014eb923c04b31c6
coelho-na <nadinecoelho9@gmail.com>
2025-06-27 16:33:05 -0300

    criando classe artista, musica e fazendo a conexão com o banco de dados

--- screensound/src/main/java/br/com/alura/screensound/model/Artista.java
+++ screensound/src/main/java/br/com/alura/screensound/model/Artista.java
@@ -2,6 +2,9 @@ package br.com.alura.screensound.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "artistas")
 public class Artista {
@@ -10,6 +13,48 @@ public class Artista {
     private Long id;
     @Column(unique = true) // definindo o nome como único, não poderá ser inserido o mesmo artista duas vezes
     private String nome;
+    @Enumerated(EnumType.STRING)
     private tipoArtista tipo;
 
+@OneToMany(mappedBy = "artista") // Um artista para várias músicas
+    private List<Musica> musicas = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public tipoArtista getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(tipoArtista tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return  " Artista ='" + nome + '\'' +
+                ", tipo=" + tipo +
+                ", musicas=" + musicas ;
+    }
 }
---
+++ screensound/src/main/java/br/com/alura/screensound/model/Musica.java
@@ -0,0 +1,44 @@
+package br.com.alura.screensound.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "musicas")
+public class Musica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto incremental
+    private Long id;
+    private String titulo;
+    @ManyToOne // Muitas musicas para um artista
+    private Artista artista;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return  "Música='" + titulo + '\'' +
+                ", artista=" + artista;
+    }
+}
